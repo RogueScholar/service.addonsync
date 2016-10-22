86d40009b31c2f3a3ae4934a6d1dc17cc4f07214
robwebset <rob@deadlyduck.com>
2016-10-22 08:24:06 +0100

    Update Links

--- README.md
+++ README.md
@@ -8,14 +8,12 @@ Please feel free to post any comments.
 
 More information can be found on the wiki:
 
-[Add-on:AddonSync](http://kodi.wiki/view/Add-on:AddonSync)
+[Add-on:AddonSync](https://github.com/robwebset/service.addonsync/wiki)
 
-You can install this addon from: [Repository:robwebset](https://github.com/robwebset/repository.robwebset/blob/master/repos/repository.robwebset/repository.robwebset-1.0.0.zip) ([wiki](http://kodi.wiki/view/Repository:robwebset))
+You can install this addon from: [Repository:robwebset](https://github.com/robwebset/repository.robwebset/blob/master/repos/repository.robwebset/repository.robwebset-1.0.0.zip) ([wiki](https://github.com/robwebset/repository.robwebset/wiki))
 
 __Donations__
 
 If you have found AddonSync useful and would like to help support future development and bug fixes, then please feel free to make a small donation.
 
 [![Donation](https://www.paypalobjects.com/en_GB/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=MPNLLJYT9ZGDA)
-
-License: See [LICENSE.txt](LICENSE.txt)
--- service.py
+++ service.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
+import sys
+import xbmc
 import xbmcaddon
+
+if sys.version_info >= (2, 7):
+    import json
+else:
+    import simplejson as json
 
 # Import the common settings
 from resources.lib.settings import log
@@ -15,12 +22,24 @@ ADDON = xbmcaddon.Addon(id='service.addonsync')
 if __name__ == '__main__':
     log("AddonSync: Service Started (version %s)" % ADDON.getAddonInfo('version'))
 
-    # Check if we should be running sync when the system starts
-    if Settings.isRunOnStartup():
-        addonSync = AddonSync()
-        addonSync.startSync()
-        del addonSync
-    else:
-        log("AddonSync: Not running at startup")
+    json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": { "addonid": "repository.robwebset", "properties": ["enabled", "broken", "name", "author"]  }, "id": 1}')
+    json_response = json.loads(json_query)
+
+    displayNotice = True
+    if ("result" in json_response) and ('addon' in json_response['result']):
+        addonItem = json_response['result']['addon']
+        if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.robwebset') and (addonItem['author'] == 'robwebset'):
+            displayNotice = False
+
+            # Check if we should be running sync when the system starts
+            if Settings.isRunOnStartup():
+                addonSync = AddonSync()
+                addonSync.startSync()
+                del addonSync
+            else:
+                log("AddonSync: Not running at startup")
+
+    if displayNotice:
+        xbmc.executebuiltin('Notification("robwebset Repository Required","github.com/robwebset/repository.robwebset",10000,%s)' % ADDON.getAddonInfo('icon'))
 
     log("AddonSync: Service Ended")
