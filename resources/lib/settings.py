d7cd3d3d150a48203a1d595e741f13bb970d8862
Peter J. Mello <admin@petermello.net>
2021-10-08 00:08:52 -0700

    Remove remaining str calls from settings.py
    Signed-off-by: Peter J. Mello <admin@petermello.net>

--- resources/lib/settings.py
+++ resources/lib/settings.py
@@ -18,28 +18,24 @@ ADDON_ID = ADDON.getAddonInfo("id")
 def log(txt, loglevel=xbmc.LOGDEBUG):
   """Control logging AddonSync messages to the Kodi system log."""
   if (ADDON.getSetting("logEnabled") == "true") or (loglevel != xbmc.LOGDEBUG):
-    if isinstance(txt, str):
-      txt = txt.decode("utf-8")
     message = f"{ADDON_ID}: {txt}"
-    xbmc.log(msg=message.encode("utf-8"), level=loglevel)
+    xbmc.log(msg=message, level=loglevel)
 
 
-def dir_exists(dirpath):
+def dir_exists(dir_path):
   """Check if a directory exists."""
-  # There is an issue with password protected smb shares, in that they seem
-  # to always return false for a directory exists call, so if we have a smb
-  # with a password and user name, then we return true
-  if "@" in dirpath:
+  # There is an issue with password protected SMB/CIFS shares, in that they
+  # seem to always return false for a directory exists call, so if we have an
+  # SMB/CIFS share with a password and user name, then we return true
+  if "@" in dir_path:
     return True
 
-  dir_path = dirpath
   # The xbmcvfs exists interface require that directories end in a slash
-  # It used to be OK not to have the slash in Gotham, but it is now required
   if (not dir_path.endswith("/")) and (not dir_path.endswith("\\")):
     dir_sep = "/"
     if "\\" in dir_path:
       dir_sep = "\\"
-    dir_path = "%s%s" % (dir_path, dir_sep)
+    dir_path = f"{dir_path}{dir_sep}"
   return xbmcvfs.exists(dir_path)
 
 
@@ -49,13 +45,12 @@ def os_path_join(folder, file):
   if folder.endswith("/") or folder.endswith("\\"):
     # Remove the slash character
     folder = folder[:-1]
-
   return os.path.join(folder, file)
 
 
 def nested_copy(root_src_dir, root_target_dir):
   """Perform a nested (recursive) copy of one directory tree to another."""
-  log("nested_copy: Copy %s to %s" % (root_src_dir, root_target_dir))
+  log(f"nested_copy: Copy {root_src_dir} to {root_target_dir}")
 
   # Make sure the target directory exists
   xbmcvfs.mkdirs(root_target_dir)
@@ -63,42 +58,32 @@ def nested_copy(root_src_dir, root_target_dir):
   dirs, files = xbmcvfs.listdir(root_src_dir)
 
   for file in files:
-    try:
-      file = file.decode("utf-8")
-    except (SyntaxError, UnicodeDecodeError):
-      pass
-    src_file = "%s%s" % (root_src_dir, file)
-    target_file = "%s%s" % (root_target_dir, file)
-    log("nested_copy: Copy file %s to %s" % (src_file, target_file))
+    src_file = f"{root_src_dir}{file}"
+    target_file = "{root_target_dir}{file}"
+    log(f"nested_copy: Copy file {src_file} to {target_file}")
     xbmcvfs.copy(src_file, target_file)
 
   for adir in dirs:
-    try:
-      adir = adir.decode("utf-8")
-    except (SyntaxError, UnicodeDecodeError):
-      pass
-    src_dir = "%s%s/" % (root_src_dir, adir)
-    target_dir = "%s%s/" % (root_target_dir, adir)
-    log("nested_copy: Copy directory %s to %s" % (src_dir, target_dir))
+    src_dir = f"{root_src_dir}{adir}/"
+    target_dir = f"{root_target_dir}{adir}/"
+    log(f"nested_copy: Copy directory {src_dir} to {target_dir}")
     nested_copy(src_dir, target_dir)
 
 
 def nested_delete(root_dir):
   """Remove a directory recursively if it is no longer needed."""
-  # If the file already exists, delete it
   if dir_exists(root_dir):
-    # Remove the png files in the directory first
     dirs, files = xbmcvfs.listdir(root_dir)
     # Remove nested directories first
-    for adir in dirs:
-      nested_delete(os_path_join(root_dir, adir))
+    for ao_dir in dirs:
+      nested_delete(os_path_join(root_dir, ao_dir))
     # If there are any nested files remove them
-    for a_file in files:
-      xbmcvfs.delete(os_path_join(root_dir, a_file))
+    for ao_file in files:
+      xbmcvfs.delete(os_path_join(root_dir, ao_file))
     # Now remove the actual directory
     xbmcvfs.rmdir(root_dir)
   else:
-    log("nested_delete: Directory %s does not exist" % root_dir)
+    log("nested_delete: Directory {root_dir} does not exist")
 
 
 class Settings:
@@ -124,9 +109,9 @@ class Settings:
     central_store_loc = ADDON.getSetting("centralStoreLocation")
     # Make sure the location ends with a slash
     if ("/" in central_store_loc) and (not central_store_loc.endswith("/")):
-      central_store_loc = "%s/" % central_store_loc
+      central_store_loc = f"{central_store_loc}/"
     elif ("\\" in central_store_loc) and (not central_store_loc.endswith("\\")):
-      central_store_loc = "%s\\" % central_store_loc
+      central_store_loc = f"{central_store_loc}\\"
     return central_store_loc
 
   @staticmethod
